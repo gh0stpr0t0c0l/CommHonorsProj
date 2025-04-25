eb5c77b82c7a451b69ba87314729c334e44829c3
gh0stpr0t0c0l <simonross@cedarville.edu>
2025-04-25 11:22:48 -0400

    added binary option to decoding script

--- parse.m
+++ parse.m
@@ -1,6 +1,9 @@
-function imageBits = parse(bitString, imageDim)
+function imageBits = parse(bitString, imageDim, binary)
     tempBits = char(bitString+'0');
     tempBits = extractAfter(tempBits,'1111000011110000');
     tempBits = tempBits(1:(imageDim^2*8*3));
+    if binary
+        tempBits = [tempBits, tempBits, tempBits];
+    end
     imageBits = tempBits - '0';
 end
