99e1f44d5e29c42792a640a683d94903893b9ffb
benpaxson <bpaxson@cedarville.edu>
2025-04-25 12:49:08 -0400

    many a change

--- decode.m
+++ decode.m
@@ -1,4 +1,7 @@
-function originalImage = decode(receivedBits, imageDim)
+function originalImage = decode(receivedBits, imageDim, binary)
+if binary
+    originalImage = reshape(receivedBits, [imageDim, imageDim]);
+else
 receivedBits = reshape(receivedBits, [8,length(receivedBits)/8]);
 receivedBits2 = zeros(1,size(receivedBits,2));
 for i = 1:size(receivedBits,2)
@@ -8,6 +11,7 @@ originalImage(:,:,1) = reshape(receivedBits2(1:(imageDim^2)),[imageDim,imageDim]
 originalImage(:,:,2) = reshape(receivedBits2((imageDim^2+1):(2*(imageDim^2))),[imageDim,imageDim]);
 originalImage(:,:,3) = reshape(receivedBits2((2*(imageDim^2)+1):(3*(imageDim^2))),[imageDim,imageDim]);
 originalImage = originalImage/255;
+end
 figure
 imagesc(originalImage)
 end
--- parse.m
+++ parse.m
@@ -1,9 +1,10 @@
 function imageBits = parse(bitString, imageDim, binary)
     tempBits = char(bitString+'0');
     tempBits = extractAfter(tempBits,'1111000011110000');
-    tempBits = tempBits(1:(imageDim^2*8*3));
     if binary
-        tempBits = [tempBits, tempBits, tempBits];
+        tempBits = tempBits(1:(imageDim^2));
+    else
+        tempBits = tempBits(1:(imageDim^2*8*3));
     end
     imageBits = tempBits - '0';
 end
